236fe4d55ec2bc9f3fee48f2919e8f9f39cf66c0
ricklucek <henriquelucek.21@gmail.com>
2022-12-26 23:03:28 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCards from "./NFTCards";
+import {CircleButton, RectButton} from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import {Subinfo} from "./Subinfo";
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCards,
+    CircleButton,
+    RectButton,
+    DetailsBid,
+    DetailsDesc,
+    Subinfo
+}
